47c5902d32ac7a1874218c1c099ee1a49d2f10c9
Pius Opoku-Fofie <pius1919@gmail.com>
2024-11-28 00:17:17 +0000

    adding the Article forms

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,9 +1,10 @@
+import ArticleForm from "@/components/ArticleForm";
 import React from "react";
 
 type Props = {};
 
 const ArticlePage = (props: Props) => {
-  return <div className="container mx-auto p-4 md:p-8" />;
+  return <div className="container mx-auto p-4 md:p-8"><ArticleForm /></div>
 };
 
 export default ArticlePage;
